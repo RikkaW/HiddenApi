a471456c60c62ba4a5921cee71de8250965b90fb
LightWayUp <lightwayuplightwayup@gmail.com>
2024-08-23 03:21:24 +0800

    Update PackageManagerHidden.java
    Add `INSTALL_BYPASS_LOW_TARGET_SDK_BLOCK` constant

--- stub/src/main/java/android/content/pm/PackageManagerHidden.java
+++ stub/src/main/java/android/content/pm/PackageManagerHidden.java
@@ -2,6 +2,8 @@ package android.content.pm;
 
 import java.util.List;
 
+import androidx.annotation.RequiresApi;
+
 import dev.rikka.tools.refine.RefineAs;
 
 @RefineAs(PackageManager.class)
@@ -10,6 +12,9 @@ public class PackageManagerHidden {
     public static int MATCH_UNINSTALLED_PACKAGES;
     public static int INSTALL_REPLACE_EXISTING;
     public static int INSTALL_ALLOW_TEST;
+
+    @RequiresApi(34)
+    public static int INSTALL_BYPASS_LOW_TARGET_SDK_BLOCK;
 
     public ApplicationInfo getApplicationInfoAsUser(String packageName, int flags, int userId) throws PackageManager.NameNotFoundException {
         throw new RuntimeException();
